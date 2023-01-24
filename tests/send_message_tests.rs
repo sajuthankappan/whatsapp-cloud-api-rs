b5f171af90ea8efbefe08253b29092e8173dcc6b
Saju Thankapan <sajuthankappan@gmail.com>
2023-01-24 16:14:15 +0000

    Add phone number id to support facebook api v15.0

--- tests/send_message_tests.rs
+++ tests/send_message_tests.rs
@@ -8,11 +8,13 @@ async fn send_text_message_works() -> Result<(), WhatsappError> {
     setup();
     let access_token = std::env::var("WHATSAPP_ACCESS_TOKEN")
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
+    let phone_number_id = std::env::var("WHATSAPP_PHONE_NUMBER_ID")
+        .expect("Missing environment variable WHATSAPP_PHONE_NUMBER_ID");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
     let text = Text::new("test message");
     let message = Message::from_text(&to, text);
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
@@ -23,13 +25,15 @@ async fn send_message_template_works() -> Result<(), WhatsappError> {
     setup();
     let access_token = std::env::var("WHATSAPP_ACCESS_TOKEN")
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
+    let phone_number_id = std::env::var("WHATSAPP_PHONE_NUMBER_ID")
+        .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
     let template_name = "hello_world";
     let language = "en_US";
     let template = Template::new(template_name, language);
     let message = Message::from_template(&to, template);
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
@@ -40,6 +44,8 @@ async fn send_message_template_with_components_works() -> Result<(), WhatsappErr
     setup();
     let access_token = std::env::var("WHATSAPP_ACCESS_TOKEN")
         .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
+    let phone_number_id = std::env::var("WHATSAPP_PHONE_NUMBER_ID")
+        .expect("Missing environment variable WHATSAPP_ACCESS_TOKEN");
     let to =
         std::env::var("WHATSAPP_SEND_TO").expect("Missing environment variable WHATSAPP_SEND_TO");
     let template_name = "sample_shipping_confirmation";
@@ -48,7 +54,7 @@ async fn send_message_template_with_components_works() -> Result<(), WhatsappErr
     let components = Vec::from([Component::with_parameters("body", parameters)]);
     let template = Template::with_components(template_name, language, components);
     let message = Message::from_template(&to, template);
-    let client = WhatasppClient::new(&access_token);
+    let client = WhatasppClient::new(&access_token, &phone_number_id);
     let response = client.send_message(&message).await?;
     assert_eq!(response.messages.len(), 1);
     Ok(())
